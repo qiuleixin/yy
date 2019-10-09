4326642680ee731f52835a8449c5a683c386475c
邱磊鑫 <qiuleixin@163.com>
2019-10-09 12:40:42 +0800

    完成了登录操作

---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-button type="info" @click='logout'>退出</el-button>
+</template>
+<script>
+    export default {
+        methods: {
+            logout() {
+                window.sessionStorage.clear('token')
+                console.log(this)
+                this.$router.push('/login')
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
